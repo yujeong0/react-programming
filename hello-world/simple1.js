8b47871d4f71b7980b824c106fe2fd00a9386e38
강유정 <kos9811@naver.com>
2021-03-21 17:29:56 +0900

    바벨 중

--- hello-world/simple1.js
+++ hello-world/simple1.js
@@ -21,36 +21,47 @@
 function LikeButton() {
     const [liked, setLiked] = React.useState(false);
     const text = liked ? '좋아요 취소' : '좋아요';
-    return React.createElement(
-        'button',
-        { onClick: () => setLiked(!liked)},
-        text,
-    );
+    return <button onClick={() => setLiked(!liked)}>{text}</button>;
+    // return React.createElement(
+    //     'button',
+    //     { onClick: () => setLiked(!liked)},
+    //     text,
+    // );
 }
 
 function Container() {
     const [count, setCount] = React.useState(0);
-    return React.createElement(
-        'div',
-        null,
-        React.createElement(LikeButton),
-        React.createElement(
-            'div',
-            { style: { marginTop: 20 } },
-            React.createElement('span', null, '현재 카운트 : '),
-            React.createElement('span', { style: { marginRight: 10 } }, count),
-            React.createElement(
-                'button',
-                { onClick: () => setCount(count + 1) },
-                '증가',
-            ),
-            React.createElement(
-                'button',
-                { onClick: () => setCount(count - 1) },
-                '감소',
-            ),
-        ),
-    )
+    return
+    <div>
+        <LikeButton />
+        <div>
+            <span>현재 카운트: </span>
+            <span style={{ marginTop: 10 }}>{count}</span>
+            <button onClick={() => setCount(count + 1)}>증가</button>
+            <button onClick={() => setCount(count - 1)}>감소</button>
+        </div>
+    </div>;
+    // return React.createElement(
+    //     'div',
+    //     null,
+    //     React.createElement(LikeButton),
+    //     React.createElement(
+    //         'div',
+    //         { style: { marginTop: 20 } },
+    //         React.createElement('span', null, '현재 카운트 : '),
+    //         React.createElement('span', { style: { marginRight: 10 } }, count),
+    //         React.createElement(
+    //             'button',
+    //             { onClick: () => setCount(count + 1) },
+    //             '증가',
+    //         ),
+    //         React.createElement(
+    //             'button',
+    //             { onClick: () => setCount(count - 1) },
+    //             '감소',
+    //         ),
+    //     ),
+    // )
 }
 
 const domContainer = document.getElementById('root');
